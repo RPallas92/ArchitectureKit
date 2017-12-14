a9a3c4fdf7912281f68f23598ed02217c8537260
rpallas92 <ricardo.pallas@adidas-group.com>
2017-12-14 17:44:14 +0100

    Initial project

---
+++ ArchitectureKit/ArchitectureKit.h
@@ -0,0 +1,19 @@
+//
+//  ArchitectureKit.h
+//  ArchitectureKit
+//
+//  Created by Pallas, Ricardo on 12/14/17.
+//  Copyright © 2017 Pallas, Ricardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ArchitectureKit.
+FOUNDATION_EXPORT double ArchitectureKitVersionNumber;
+
+//! Project version string for ArchitectureKit.
+FOUNDATION_EXPORT const unsigned char ArchitectureKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArchitectureKit/PublicHeader.h>
+
+
